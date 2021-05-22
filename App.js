99c217c9a6ef9f25f07ddd6b97e1f75ada6683b1
upljoyitaseal <joyita.seal@utplco.com>
2021-05-22 15:21:40 +0530

    implemented redux

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 import HomeScreen from './Screen/HomeScreen'
 import ViewScreen from './Screen/ViewScreen'
-const App = () =>
-{
+import TodoReducers from './Screen/TodoReducers'
+ import { Provider } from 'react-redux'
+ import { creatStore } from 'redux'
+
+// const initialState = {
+// todo_list =[]
+// }
+// const reducer = (state = initialState,action) => {
+//   return state;
+// }
+
+ const store = creatStore( )
+const App = () => {
   return (
+    render(
+          <Provider store = {store}>
+          <ViewScreen/>
+        </Provider>
+    ),
     <NavigationContainer>
       <Stack.Navigator initialRouteName="HomeScreen">
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="ViewScreen" component={ViewScreen} />
       </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
+    
   );
 }
 
--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { ActivityIndicator, StyleSheet, Text, View, SafeAreaView, TextInput, Image, TouchableOpacity, Alert } from "react-native"
 import { NavigationContainer } from '@react-navigation/native';
-
+import {useState} from 'react'
+import {useSelector,useDispatch} from "react-redux"
+//import{addTodo,deleteTodo, removeTodo } from './Screen/Index'
 const HomeScreen = ({ navigation }) =>
 {
+const [inputData, setInputData] = useState('');
+const list = useSelector((state=> state.TodoReducers.list));
+const dispatch = useDispatch();
   return (
       <View style={[styles.container], { flex: 1, backgroundColor: 'dodgerblue' }}>
           <View style={styles.container1}>
               <TextInput keyboardType='twitter'
                   placeholder="Add your Task Here"
-                  multiline={true}
+                  value ={ inputDta}
+                  onChangeText={(inputData) => setInputData(inputData)}
                   textAlign='center'
                   style={styles.input}
+
                 >  
               </TextInput>
               <TouchableOpacity style={[styles.button]}  >
-              <Text style={{ textAlign: "center" }}>Add to your Task</Text>
+              <Text style={{ textAlign: "center" }} onClick ={() => dispatch(addTodo(inputData),setInputData(''))}>Add to your Task</Text>
               </TouchableOpacity>
               <TouchableOpacity style={[styles.button1]}
                   onPress=
@@ -24,6 +31,7 @@ const HomeScreen = ({ navigation }) =>
                       { textAlign: "center" }}>
                       Go to View your task</Text>
               </TouchableOpacity>
+              
                    </View>
     </View>
   );
@@ -47,8 +55,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     alignSelf:'center',
         position: 'absolute',
-        top: 400,
-    marginBottom:50,
+    top:400
     
     },
     button1:
@@ -60,10 +67,21 @@ const styles = StyleSheet.create({
           justifyContent: 'center',
       alignSelf:'center',
           position: 'absolute',
-        top: 450,
-      marginBottom:50
+      top:450
       
     },
+    button2:
+    {
+      backgroundColor: 'gray',
+      margin: 40,
+          height: 40,
+      width:310,
+          justifyContent: 'center',
+      alignSelf:'center',
+          position: 'absolute',
+      top:500
+      
+      },
     input:
   {
     height: 400,
@@ -72,9 +90,7 @@ const styles = StyleSheet.create({
     position: 'relative',
     bottom: 10,
     width: 310,
-        margin: 30,
-        fontSize: 20,
-       fontWeight:'bold'
+    margin: 30,
   },
 });
 
